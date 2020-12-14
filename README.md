8b241d77265bd8585184195840f1e02591be7f4a
Plinio Mikael <pliniomikael@gmail.com>
2020-12-13 21:49:11 -0300

    finish

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# hacka_ambev
- Project To Hackaton Ambev
+## 💻 Projeto
+
+A Beer Check é uma inovação tecnológica que utiliza sensoriamento em cascata a fibra óptica para criar a cerveja na medida certa.  Nossos sensores dispõem de características específicas que nenhuma outra tecnologia pode oferecer, como: análise simultânea do teor alcoólico, CO² e O² dissolvido no líquido em altas temperatura acima de 90º, além disso, nossa solução é de baixo custo e monitoramento remoto.
+
+
+[Link para o Front-end](https://github.com/itsaleplets/hackathonAmbev)
+
+## :hammer: Instalação
+
+Com o Python >= 3.0 instalado
+
+Arquivo arduino Uno chamado:
+
+```
+Ambev_Havk_V2.ino
+```
+
+Dentro da pasta do projeto execute
+
+Instalação dos pacotes:
+
+```
+pip install -r requirements.txt
+```
+
+Rodando a Aplicação:
+
+```
+python app.py
+```
+
--- app.py
+++ app.py
@@ -8,50 +8,37 @@ import serial
 app = Flask(__name__)
 CORS(app)
 
-result = {
-    "5": "1532,45",
-    "4,70": "1533,03",
-    "4,40": "1533,62",
-    "4,10": "1534,18",
-    "3,80": "1534,75",
-    "3,50": "1535,33",
-    "3,20": "1535,92",
-    "2,90": "1536,5",
-    "2,60": "1537,05",
-}
-
 
 @app.before_first_request
 def setupSerial():
     global arduino
     """ subistituir por /COM/ alguma coisa da porta no windows -  /dev/tty.usbmodem411 """
 
-    # descomentar todos os arduinos
-    # arduino = serial.Serial("COM5", 9600, timeout=1)
+    arduino = serial.Serial("COM5", 9600, timeout=1)
 
 
 @app.route("/")
 def index():
-    # print(result["5"])
+
     now = datetime.now()
     data = now.strftime("%d/%m/%Y")
     hora = now.strftime("%H:%M:%S")
-    # value = arduino.readline()
-    # alcool = str(value)
+    value = arduino.readline()
+    alcool = str(value)
 
     return jsonify(
         {
             "date": data,
             "time": hora,
-            "title": "Alcool",
-            "percent": "alcool",
+            "title": "Teor Alcoólico",
+            "percent": alcool,
         }
     )
 
 
 @atexit.register
 def disconect():
-    # arduino.closePort()
+    arduino.closePort()
     print("Close Port")
 
 
